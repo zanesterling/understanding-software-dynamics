e7fd4d9167b1c318863ca529d5e2d44886c6e05e
Zane Sterling <sterling.zane@gmail.com>
2024-06-21 22:50:28 +0200

    Also measure columnwise multiply

--- ch4-cpu-memory-interactions/interaction.cc
+++ ch4-cpu-memory-interactions/interaction.cc
@@ -70,6 +70,28 @@ double SimpleMatMul(Matrix a, Matrix b, Matrix c) {
   return (stop - start) * 1. / CLOCKS_PER_SEC;
 }
 
+// c = a*b
+//
+// Returns the number of seconds taken.
+double SimpleMatMulColumnwise(Matrix a, Matrix b, Matrix c) {
+  assert(a.width == b.height);
+  assert(a.height == c.height);
+  assert(b.width == c.width);
+
+  clock_t start = clock();
+  for (size_t col = 0; col < b.width; ++col) {
+    for (size_t row = 0; row < a.height; ++row) {
+      double sum = 0;
+      for (size_t k = 0; k < a.width; ++k) {
+        sum += a.data[row * a.width + k] * b.data[k * b.width + col];
+      }
+      c.data[row * c.width + col] = sum;
+    }
+  }
+  clock_t stop = clock();
+  return (stop - start) * 1. / CLOCKS_PER_SEC;
+}
+
 int main(int argc, char** argv) {
   Matrix a = MakeMatrix(1024, 1024);
   Matrix b = MakeMatrix(1024, 1024);
@@ -83,8 +105,17 @@ int main(int argc, char** argv) {
   fprintf(stdout, "pre: sizeof(Matrix.data)=%lu\n", sizeof(a.data[0]) * a.width * a.height);
   fprintf(stdout, "\n");
 
-  CleanCache();
-  double secs = SimpleMatMul(a, b, c);
-  double checksum = Checksum(c);
-  fprintf(stdout, "SimpleMatMul: %f seconds, sum = %f\n", secs, checksum);
+  {
+    CleanCache();
+    double secs = SimpleMatMul(a, b, c);
+    double checksum = Checksum(c);
+    fprintf(stdout, "SimpleMatMul:           %f seconds, sum = %f\n", secs, checksum);
+  }
+
+  {
+    CleanCache();
+    double secs = SimpleMatMulColumnwise(a, b, c);
+    double checksum = Checksum(c);
+    fprintf(stdout, "SimpleMatMulColumnwise: %f seconds, sum = %f\n", secs, checksum);
+  }
 }
