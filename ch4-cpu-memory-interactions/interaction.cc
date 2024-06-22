db5195dd74938ff86e0f52b0445c1d659ffff07e
Zane Sterling <sterling.zane@gmail.com>
2024-06-22 15:19:26 +0200

    Add transpose method

--- ch4-cpu-memory-interactions/interaction.cc
+++ ch4-cpu-memory-interactions/interaction.cc
@@ -77,6 +77,50 @@ double SimpleMatMulColumnwise(Matrix a, Matrix b, Matrix c) {
   return (stop - start) * 1. / CLOCKS_PER_SEC;
 }
 
+// Returns the transpose of the given matrix.
+// Involves a malloc.
+Matrix Transposed(Matrix a) {
+  const size_t n = a.width * a.height;
+  double* const data = (double*)aligned_alloc(PAGE_SIZE, n * sizeof(double));
+  Matrix ret = {data, a.height, a.width};
+  for (size_t y = 0; y < a.height; y++) {
+    for (size_t x = 0; x < a.width; x++) {
+      double val = a.data[y * a.width + x];
+      ret.data[x * a.height + y] = val;
+    }
+  }
+  return ret;
+}
+
+// c = a*b
+//
+// Returns the number of seconds taken.
+double TransposeMatMul(Matrix a, Matrix b, Matrix* c) {
+  assert(a.width == b.height);
+  assert(a.height == c->height);
+  assert(b.width == c->width);
+
+  clock_t start = clock();
+  Matrix bb = Transposed(b);
+  Matrix cc = {c->data, c->height, c->width};
+  for (size_t col = 0; col < b.width; ++col) {
+    for (size_t row = 0; row < a.height; ++row) {
+      double sum = 0;
+      for (size_t k = 0; k < a.width; ++k) {
+        sum += a.data[row * a.width + k] * bb.data[col * bb.width + k];
+      }
+      cc.data[row * cc.width + col] = sum;
+    }
+  }
+  Matrix ccc = Transposed(cc);
+  free(c->data);
+  c->data = ccc.data;
+  free(bb.data);
+
+  clock_t stop = clock();
+  return (stop - start) * 1. / CLOCKS_PER_SEC;
+}
+
 int main(int argc, char** argv) {
   Matrix a = MakeMatrix(1024, 1024);
   Matrix b = MakeMatrix(1024, 1024);
@@ -108,6 +152,15 @@ int main(int argc, char** argv) {
     double secs = SimpleMatMulColumnwise(a, b, c);
     double checksum = Checksum(c);
     fprintf(stdout, "SimpleMatMulColumnwise: %f seconds, sum = %f\n", secs, checksum);
+  }
+  #endif
+
+  #if defined TRANSPOSE || defined ALL
+  {
+    CleanCache();
+    double secs = TransposeMatMul(a, b, &c);
+    double checksum = Checksum(c);
+    fprintf(stdout, "TransposeMatMul: %f seconds, sum = %f\n", secs, checksum);
   }
   #endif
 }
