ce19bc65cc7cf9dd596dbcbb648c3cc66e41f855
Zane Sterling <sterling.zane@gmail.com>
2024-06-23 14:47:28 +0200

    Fix bug in BlockUnremap and switch to 8x8 blocks

--- ch4-cpu-memory-interactions/interaction.cc
+++ ch4-cpu-memory-interactions/interaction.cc
@@ -181,28 +181,30 @@ double TransposeMatMul(Matrix a, Matrix b, Matrix* c) {
   return (stop - start) * 1. / CLOCKS_PER_SEC;
 }
 
-// Map from row-major to 32x32block-major.
+constexpr size_t kBlockDim = 8;
+constexpr size_t kBlockArea = kBlockDim * kBlockDim;
+
+// Map from row-major to 16x16block-major.
 Matrix BlockRemap(Matrix a) {
-  assert(a.height % 32 == 0);
-  assert(a.width % 32 == 0);
+  assert(a.height % kBlockDim == 0);
+  assert(a.width % kBlockDim == 0);
 
   double* const data = (double*)aligned_alloc(PAGE_SIZE, a.width * a.height * sizeof(double));
   Matrix ret = {data, a.height, a.width};
 
-  const size_t block_w = a.width  / 32;
-  const size_t block_h = a.height / 32;
-  const size_t block_size = 32 * 32;
+  const size_t block_w = a.width  / kBlockDim;
+  const size_t block_h = a.height / kBlockDim;
   for (size_t a_block_y = 0; a_block_y < block_h; ++a_block_y) {
     for (size_t a_block_x = 0; a_block_x < block_w; ++a_block_x) {
       const size_t ret_block = a_block_y * block_w + a_block_x;
-      double* const block_data = ret.data + ret_block * block_size;
+      double* const block_data = ret.data + ret_block * kBlockArea;
 
-      for (size_t row = 0; row < 32; ++row) {
-        const size_t a_y = a_block_y * 32 + row;
+      for (size_t row = 0; row < kBlockDim; ++row) {
+        const size_t a_y = a_block_y * kBlockDim + row;
         double* const a_row = a.data + a_y * a.width;
-        for (size_t col = 0; col < 32; ++col) {
-          const size_t a_x = a_block_x * 32 + col;
-          block_data[row * 32 + col] = a_row[a_x];
+        for (size_t col = 0; col < kBlockDim; ++col) {
+          const size_t a_x = a_block_x * kBlockDim + col;
+          block_data[row * kBlockDim + col] = a_row[a_x];
         }
       }
     }
@@ -211,27 +213,26 @@ Matrix BlockRemap(Matrix a) {
   return ret;
 }
 
-// Map from 32x32block-major to row-major.
+// Map from 16x16block-major to row-major.
 Matrix BlockUnmap(Matrix a) {
-  assert(a.height % 32 == 0);
-  assert(a.width % 32 == 0);
+  assert(a.height % kBlockDim == 0);
+  assert(a.width % kBlockDim == 0);
 
   double* const data = (double*)aligned_alloc(PAGE_SIZE, a.width * a.height * sizeof(double));
   Matrix ret = {data, a.height, a.width};
 
-  const size_t block_w = a.width  / 32;
-  const size_t block_h = a.height / 32;
-  const size_t block_size = 32 * 32;
+  const size_t block_w = a.width  / kBlockDim;
+  const size_t block_h = a.height / kBlockDim;
 
-  for (size_t block_y = 0; block_y < block_h; ++block_y) {
+  for (size_t block_y = 0;   block_y < block_h; ++block_y) {
     for (size_t block_x = 0; block_x < block_w; ++block_x) {
-      const size_t block_i = block_y * block_w + block_x;
-      double* const a_block = a.data + block_i * block_size;
-      for (size_t row = 0; row < 32; ++row) {
-        for (size_t col = 0; col < 32; ++col) {
-          const size_t ret_x = block_x * 32 + col;
-          const size_t ret_y = block_y * 32 + row;
-          ret.data[ret_y * ret.width + ret_x] = a_block[row * 32 + col];
+      double* const a_block = a.data + kBlockArea * (block_y * block_w + block_x);
+
+      for (size_t row = 0; row < kBlockDim; ++row) {
+        for (size_t col = 0; col < kBlockDim; ++col) {
+          const size_t ret_x = block_x * kBlockDim + col;
+          const size_t ret_y = block_y * kBlockDim + row;
+          ret.data[ret_y * ret.width + ret_x] = a_block[row * kBlockDim + col];
         }
       }
     }
@@ -256,60 +257,34 @@ double BlockRemapMatMul(Matrix a, Matrix b, Matrix* c) {
   // for (int i = 0; i < cc.height * cc.width; ++i) cc.data[i] = 0.0;
   memset(cc.data, 0, cc.height * cc.width * sizeof(double));
 
-  constexpr size_t BLOCK_DIM = 32;
-  constexpr size_t BLOCK_AREA = BLOCK_DIM * BLOCK_DIM;
-  const size_t c_block_w = cc.width  / BLOCK_DIM;
-  const size_t c_block_h = cc.height / BLOCK_DIM;
+  const size_t c_block_w = cc.width  / kBlockDim;
+  const size_t c_block_h = cc.height / kBlockDim;
 
-  for (size_t block_row = 0; block_row < cc.height / BLOCK_DIM; ++block_row) {
-    for (size_t block_col = 0; block_col < cc.width / BLOCK_DIM; ++block_col) {
-      double* const cc_block = cc.data + BLOCK_AREA * (block_row * c_block_w + block_col);
+  for (size_t block_row = 0;   block_row < c_block_h; ++block_row) {
+    for (size_t block_col = 0; block_col < c_block_w; ++block_col) {
+      double* const cc_block = cc.data + kBlockArea * (block_row * c_block_w + block_col);
 
       // Produce cc_block.
-      for (size_t k = 0; k < a.width / BLOCK_DIM; ++k) {
+      for (size_t k = 0; k < a.width / kBlockDim; ++k) {
         // Pseudocode:
         //   const Block aa_block = aa.blocks[block_row * c_block_w + k];
         //   const Block bb_block = aa.blocks[k         * c_block_w + block_col];
         //   cc.block += aa_block * bb_block;
-        double* const aa_block = aa.data + BLOCK_AREA * (block_row * c_block_w + k);
-        double* const bb_block = bb.data + BLOCK_AREA * (k         * c_block_w + block_col);
-        // Multiply these two 32x32 row-major matrices into cc_block.
-        for (size_t row = 0; row < 32; ++row) {
-          for (size_t col = 0; col < 32; ++col) {
+        double* const aa_block = aa.data + kBlockArea * (block_row * c_block_w + k);
+        double* const bb_block = bb.data + kBlockArea * (k         * c_block_w + block_col);
+        // Multiply these two 16x16 row-major matrices into cc_block.
+        for (size_t row = 0; row < kBlockDim; ++row) {
+          for (size_t col = 0; col < kBlockDim; ++col) {
             double sum = 0.0;
-            sum += aa_block[row * 32 +  0] * bb_block[ 0 * 32 + col];
-            sum += aa_block[row * 32 +  1] * bb_block[ 1 * 32 + col];
-            sum += aa_block[row * 32 +  2] * bb_block[ 2 * 32 + col];
-            sum += aa_block[row * 32 +  3] * bb_block[ 3 * 32 + col];
-            sum += aa_block[row * 32 +  4] * bb_block[ 4 * 32 + col];
-            sum += aa_block[row * 32 +  5] * bb_block[ 5 * 32 + col];
-            sum += aa_block[row * 32 +  6] * bb_block[ 6 * 32 + col];
-            sum += aa_block[row * 32 +  7] * bb_block[ 7 * 32 + col];
-            sum += aa_block[row * 32 +  8] * bb_block[ 8 * 32 + col];
-            sum += aa_block[row * 32 +  9] * bb_block[ 9 * 32 + col];
-            sum += aa_block[row * 32 + 10] * bb_block[10 * 32 + col];
-            sum += aa_block[row * 32 + 11] * bb_block[11 * 32 + col];
-            sum += aa_block[row * 32 + 12] * bb_block[12 * 32 + col];
-            sum += aa_block[row * 32 + 13] * bb_block[13 * 32 + col];
-            sum += aa_block[row * 32 + 14] * bb_block[14 * 32 + col];
-            sum += aa_block[row * 32 + 15] * bb_block[15 * 32 + col];
-            sum += aa_block[row * 32 + 16] * bb_block[16 * 32 + col];
-            sum += aa_block[row * 32 + 17] * bb_block[17 * 32 + col];
-            sum += aa_block[row * 32 + 18] * bb_block[18 * 32 + col];
-            sum += aa_block[row * 32 + 19] * bb_block[19 * 32 + col];
-            sum += aa_block[row * 32 + 20] * bb_block[20 * 32 + col];
-            sum += aa_block[row * 32 + 21] * bb_block[21 * 32 + col];
-            sum += aa_block[row * 32 + 22] * bb_block[22 * 32 + col];
-            sum += aa_block[row * 32 + 23] * bb_block[23 * 32 + col];
-            sum += aa_block[row * 32 + 24] * bb_block[24 * 32 + col];
-            sum += aa_block[row * 32 + 25] * bb_block[25 * 32 + col];
-            sum += aa_block[row * 32 + 26] * bb_block[26 * 32 + col];
-            sum += aa_block[row * 32 + 27] * bb_block[27 * 32 + col];
-            sum += aa_block[row * 32 + 28] * bb_block[28 * 32 + col];
-            sum += aa_block[row * 32 + 29] * bb_block[29 * 32 + col];
-            sum += aa_block[row * 32 + 30] * bb_block[30 * 32 + col];
-            sum += aa_block[row * 32 + 31] * bb_block[31 * 32 + col];
-            cc_block[row * 32 + col] += sum;
+            sum += aa_block[row * kBlockDim +  0] * bb_block[ 0 * kBlockDim + col];
+            sum += aa_block[row * kBlockDim +  1] * bb_block[ 1 * kBlockDim + col];
+            sum += aa_block[row * kBlockDim +  2] * bb_block[ 2 * kBlockDim + col];
+            sum += aa_block[row * kBlockDim +  3] * bb_block[ 3 * kBlockDim + col];
+            sum += aa_block[row * kBlockDim +  4] * bb_block[ 4 * kBlockDim + col];
+            sum += aa_block[row * kBlockDim +  5] * bb_block[ 5 * kBlockDim + col];
+            sum += aa_block[row * kBlockDim +  6] * bb_block[ 6 * kBlockDim + col];
+            sum += aa_block[row * kBlockDim +  7] * bb_block[ 7 * kBlockDim + col];
+            cc_block[row * kBlockDim + col] += sum;
           }
         }
       }
