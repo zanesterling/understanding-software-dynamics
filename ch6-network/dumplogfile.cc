fe2d2e38e0d55600af42e9f4fce0e3ed211db345
Zane Sterling <sterling.zane@gmail.com>
2024-07-11 19:54:24 +0200

    Make dumplogfile output JSON

--- ch6-network/dumplogfile.cc
+++ ch6-network/dumplogfile.cc
@@ -1,7 +1,10 @@
 #include <assert.h>
+#include <inttypes.h>
 #include <fcntl.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/param.h>
 #include <unistd.h>
 
 #include "rpc.h"
@@ -22,6 +25,32 @@ struct LogMessage {
 
 static_assert(96 == sizeof(LogMessage));
 
+void myputs(FILE* fd, const char* s) {
+  for (; *s; ++s) fputc(*s, fd);
+}
+
+void fhexdumpn(FILE* fd, const char* s, size_t n) {
+  for (size_t i = 0; i < n; i += 8) {
+    for (size_t j = 0; j < 8; ++j) {
+      if (i+j >= n) {
+        myputs(fd, "   ");
+      } else {
+        fprintf(fd, "%02x ", s[i+j]);
+      }
+    }
+    fputc('|', fd);
+    for (size_t j = 0; j < 8 && i+j < n; ++j) {
+      if (j < 0x20 || j > 0x7e) {
+        // Nonprintable character, so print a '.'.
+        fputc('.', fd);
+      } else {
+        fputc(s[i+j], fd);
+      }
+    }
+    myputs(fd, "|\\n");
+  }
+}
+
 int main(int argc, char** argv) {
   if (argc != 2) usage(argv), exit(1);
 
@@ -34,6 +63,8 @@ int main(int argc, char** argv) {
   // Logs are arranged as MARK HEADER BODY_TRUNC,
   // where BODY_TRUNC is 24 bytes of truncated or zero-extended data.
 
+  putchar('[');
+  bool is_first_message = true;
   while (true) {
     LogMessage message;
     ssize_t n_bytes = read(log_fd, &message, sizeof(LogMessage));
@@ -51,9 +82,67 @@ int main(int argc, char** argv) {
       exit(1);
     }
 
-    message.header.pretty_print();
-    printf("body: %s\n\n", message.body);
+    if (!is_first_message) {
+      myputs(stdout, ",\n");
+    }
+    is_first_message = false;
+
+    printf(
+      "{\n"
+      "\t\"rpc_id\":      %d,\n"
+      "\t\"parent\":      %d,\n"
+      "\t\"t1_us\":       %" PRIu64 ",\n"
+      "\t\"t2_us\":       %" PRIu64 ",\n"
+      "\t\"t3_us\":       %" PRIu64 ",\n"
+      "\t\"t4_us\":       %" PRIu64 ",\n"
+      "\t\"client\":      \"%d.%d.%d.%d:%d\",\n"
+      "\t\"server\":      \"%d.%d.%d.%d:%d\",\n"
+      "\t\"req_len_log\": %u,\n"
+      "\t\"res_len_log\": %u,\n",
+
+      message.header.rpc_id,
+      message.header.parent,
+      message.header.req_send_time_us,
+      message.header.req_recv_time_us,
+      message.header.res_send_time_us,
+      message.header.res_recv_time_us,
+
+      (message.header.client_ip & 0xff000000) >> 24,
+      (message.header.client_ip & 0x00ff0000) >> 16,
+      (message.header.client_ip & 0x0000ff00) >> 8,
+       message.header.client_ip & 0x000000ff,
+       message.header.client_port,
+      (message.header.server_ip & 0xff000000) >> 24,
+      (message.header.server_ip & 0x00ff0000) >> 16,
+      (message.header.server_ip & 0x0000ff00) >> 8,
+       message.header.server_ip & 0x000000ff,
+       message.header.server_port,
+
+      message.header.req_len_log,
+      message.header.res_len_log
+    );
+
+    myputs(stdout, "\t\"type\":     \"");
+    const char* type_str = message_type_str(message.header.message_type);
+    fhexdumpn(stdout, type_str, strlen(type_str));
+    myputs(stdout, "\",\n");
+
+    myputs(stdout, "\t\"method\":   \"");
+    fhexdumpn(stdout, message.header.method, 8);
+    myputs(stdout, "\",\n");
+
+    myputs(stdout, "\t\"status\":   \"");
+    const char* stat_str = status_str(message.header.status);
+    fhexdumpn(stdout, stat_str, strlen(stat_str));
+    myputs(stdout, "\",\n");
+
+    myputs(stdout, "\t\"body\":     \"");
+    fhexdumpn(stdout, (char*)message.body, 24);
+    myputs(stdout, "\"\n");
+
+    myputs(stdout, "}");
   }
+  myputs(stdout, "]\n");
 
   return 0;
 }
